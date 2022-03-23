018fe1f81e6241565f6d99b4df0b907563c7c7b8
Max Zhurkin <tryzoloft@gmail.com>
2022-03-23 13:23:11 +0500

    added tags

---
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -0,0 +1,26 @@
+package properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("properties")
+public class SystemPropertiesTests {
+    @Test
+    void someTest1() {
+        String browser = System.getProperty("browser");
+        System.out.println(browser); // null
+
+    }
+    @Test
+    void someTest2() {
+        System.setProperty("some_browser", "safari");
+        String browser = System.getProperty("browser");
+        System.out.println(browser); //safari
+    }
+    @Test
+    @Tag("smoke")
+    void someTest3() {
+        String browser = System.getProperty("browser", "opera");
+        System.out.println(browser); // opera
+    }
+}
